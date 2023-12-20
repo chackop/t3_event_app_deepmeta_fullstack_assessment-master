852a481a609d061f105d4e99a28b697d01162ac2
Chacs <chackop@gmail.com>
2023-12-20 00:41:44 +0000

    added page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,7 @@
-import { LoadingPage } from "~/components/loading/loading";
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { useState } from "react";
+import { LoadingPage, LoadingSpinner } from "~/components/loading/loading";
 import { api } from "~/utils/api";
 
 const EventFeed = () => {
@@ -22,6 +25,56 @@ const EventFeed = () => {
   );
 };
 
+const EventItemCreation = () => {
+  const [input, setInput] = useState("");
+
+  const ctx = api.useContext();
+
+  const { mutate, isLoading } = api.post.create.useMutation({
+    onSuccess: () => {
+      setInput("");
+      void ctx.count.getAll.invalidate();
+    },
+  });
+
+  return (
+    <div>
+      <input
+        placeholder="Enter Event Title"
+        className="grow bg-transparent outline-none"
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            e.preventDefault();
+
+            if (input !== "") {
+              mutate({ title: input });
+            }
+          }
+        }}
+        disabled={isLoading}
+      />
+
+      {input !== "" && !isLoading && (
+        <button
+          onClick={() => mutate({ title: input })}
+          class="rounded bg-gray-500 px-4 py-2 font-bold text-white hover:bg-gray-700"
+        >
+          Submit
+        </button>
+      )}
+
+      {isLoading && (
+        <div className="flex items-center justify-center">
+          <LoadingSpinner size={20} />
+        </div>
+      )}
+    </div>
+  );
+};
+
 export default function Home() {
   return (
     <>
@@ -30,6 +83,8 @@ export default function Home() {
           <h1 className="text-xl font-medium">Welcome To Event App!</h1>
 
           <br />
+
+          <EventItemCreation />
 
           <EventFeed />
         </div>
